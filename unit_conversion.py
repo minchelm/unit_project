07ec28dab0e056097802af262d428df58c57f665
minchelm <mitchelmckenrick@gmail.com>
2026-02-21 14:45:02 +0000

    conversion function creation
    takes the user input, runs through input_to_list, gets value, initial units, final units. returns as string for output in window.
    errors detectable:
    - if the user's input is too short, e.g. 1 (units missing)
    - if the units are unknown
    - if the value is a string, it is acknowledged as in the wrong format.

--- unit_conversion.py
+++ unit_conversion.py
@@ -15,3 +15,42 @@ def input_to_list(user_input):
         return value, units_from, units_to
 
 
+def conversion(user_input):
+    value, units_from, units_to = input_to_list(user_input)
+    if value is None:
+        return "Not enough values"
+    if units_from != -1:
+        try:
+            print_test = False  # for parsing through dictionaries
+            for (
+                dictionary
+            ) in dictionaries:  # goes through the dictionaries (currently only two)
+                if (  # if either of the unit values are unknown
+                    dictionary.get(units_from) is None
+                    or dictionary.get(units_to) is None
+                ):
+                    continue
+                else:  # if both units are in one dictionary, converts
+                    units_standard = (
+                        dictionary.get(units_from) * value
+                    )  # converts to standard value (m, kg)
+                    unit_final = units_standard / dictionary.get(
+                        units_to
+                    )  # converts standard value to final value
+                    return (
+                        units_from
+                        + " to "
+                        + units_to
+                        + ": "
+                        + str(unit_final)
+                        + " "
+                        + units_to
+                    )  # prints desired value and units
+                    print_test = True  # notifies if conversion happens
+                    return True
+            if (
+                print_test is False
+            ):  # if no print happens, the units were unknown somehow
+                return "Unknown units"
+        except ValueError:
+            return "Incorrect format: '<value> <initial units> <final units>'"
